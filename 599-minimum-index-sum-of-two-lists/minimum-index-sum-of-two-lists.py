1d781b774c40a658c1d5c0dfac75bbbd52883243
Sankar B <sankarper0907@gmail.com>
2026-07-11 17:36:30 +0530

    Time: 958 ms (5.15%) | Memory: 12.5 MB (71.55%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def findRestaurant(self, list1, list2):
 
-        min_sum = float("inf")
-        result = []
+        min_sum=float("inf")
+        result=[]
 
         for i in range(len(list1)):
             for j in range(len(list2)):
-                if list1[i] == list2[j]:
-
-                    total = i + j
-
-                    if total < min_sum:
+                total=i+j
+                if list1[i]==list2[j]:
+                    if total<min_sum:
                         min_sum=total
                         result=[list1[i]]
-                    elif total == min_sum:
+                        
+                    elif total==min_sum:
                         result.append(list1[i])
-
         return result
